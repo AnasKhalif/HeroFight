18e57f8465c2104881283a508d7b3f09a0e98883
AnasKhalif <anaskhalif995@gmail.com>
2024-06-06 11:00:21 +0700

    add file main output HeroFight java

---
+++ src/main/java/com/mycompany/herofight/HeroFight.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.herofight;
+
+/**
+ *
+ * @author anaskhalifm
+ */
+public class HeroFight {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
